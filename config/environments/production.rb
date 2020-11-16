d4bad42646bb88aef109e083c6a451aeb08787eb
Walter de Camargo Grangeiro <walter.camargo.gr@gmail.com>
2020-11-16 14:13:47 -0300

    Add line :smtp config in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ############## CONFIGURAÇÃO HOST
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {host: ENV['MAILER_DOMAIN'] }
   config.action_mailer.delivery_method = :smtp
 
