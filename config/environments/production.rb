3ecd839098076ddf1909a58841e2491ff4a93405
Walter de Camargo Grangeiro <walter.camargo.gr@gmail.com>
2020-11-16 16:35:56 -0300

    Add MailGun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => ENV['MAILER_DOMAIN'] ,
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
