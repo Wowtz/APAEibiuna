0f1fa274a26adf3b39e59277f61335780a6fab8d
Walter de Camargo Grangeiro <walter.camargo.gr@gmail.com>
2020-11-17 10:54:28 -0300

    Provisory form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   ####
   config.action_mailer.default_url_options = {host: "localhost:3000", port: 3000}
   config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
     :address        => "smtp.gmail.com",
